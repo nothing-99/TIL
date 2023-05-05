fac0478bcc9eea09c4915338bcda3468b374a9af
rareapepe <sanghun-kim@naver.com>
2023-05-05 22:40:31 +0900

    [자료구조] (최대|최소)이진힙 - 이진힙은 부모요소와 자식요소사이의 관계에 따라 최소이진힙, 최대이진힙으로 나눌 수 있다. - 이때 부모-자식 요소 간에는 대소관계가 성립하지만 자식 요소 간에는 대소관계가 없다. 이진 탐색 트리와 다른 점이다. - insert, extract 는 O(logN)의 시간복잡도를 가지고 search 는 O(N)의 시간복잡도를 가진다. O(N)의 시간복잡도를 가지는 이유는 자식 노드간에 대소관계가 없기에 최대 모든 노드를 봐야하기 때문이다. 고로 탐색을 위해서는 이진 탐색 트리 사용을 고려하자

---
+++ cs/data_structure/MaxBinaryHeap.js
@@ -0,0 +1,58 @@
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(newData) {
+    // 말단에 위치시킨다
+    let newDataIdx = this.values.length;
+    let comparedIdx = Math.floor((newDataIdx - 1) / 2);
+    // 부모요소와 비교
+    // 부모요소가 더 작을 경우 스왑
+    while (newDataIdx > 0 && this.values[comparedIdx] < newData) {
+      this.values[newDataIdx] = this.values[comparedIdx];
+      [newDataIdx, comparedIdx] = [comparedIdx, Math.floor((comparedIdx - 1) / 2)];
+    }
+    this.values[newDataIdx] = newData;
+  }
+  extractMin() {
+    // 가장 큰 값이 보장된 루트 요소
+    const extracted = this.values.length ? this.values[0] : null;
+    if (this.values.length === 1) this.values.pop();
+    if (this.values.length > 1) {
+      // 말단 요소 루트로 올리기
+      this.values[0] = this.values.pop();
+      // 말단 요소 올바른 곳에 위치시키기
+      this.sinkDown();
+    }
+    return extracted;
+  }
+  sinkDown() {
+    const cur = this.values[0];
+    let idx = 0;
+    let swap = null;
+
+    // 왼쪽 자식 요소가 존재할 경우
+    while (Math.floor(idx * 2 + 1) < this.values.length) {
+      const leftIdx = Math.floor(idx * 2 + 1);
+      const rightIdx = Math.floor(idx * 2 + 2);
+      const left = this.values[leftIdx];
+
+      // 기준 요소와 왼쪽 요소의 비교
+      if (left > cur) swap = leftIdx;
+      // 오른쪽 요소 존재 여부 확인
+      if (rightIdx < this.values.length) {
+        const right = this.values[rightIdx];
+        // 왼쪽 요소와 오른쪽 요소의 비교
+        if (swap && right > left) swap = rightIdx;
+        // 기준 요소와 오른쪽 요소의 비교
+        if (!swap && right > cur) swap = rightIdx;
+      }
+      // 현재 위치가 올바른 위치
+      if (!swap) break;
+      // 스왑
+      this.values[idx] = this.values[swap];
+      [idx, swap] = [swap, null];
+    }
+    this.values[idx] = node;
+  }
+}
